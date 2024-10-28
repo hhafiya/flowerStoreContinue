7f6edcd5a9612aeb982d8aac6f17f3f93a0fb242
hhafiya <149326005+hhafiya@users.noreply.github.com>
2024-10-28 20:34:41 +0200

    Update FlowerContinueApplication.java

--- src/main/java/com/flower/flowerсontinue/FlowerContinueApplication.java
+++ src/main/java/com/flower/flowerсontinue/FlowerContinueApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FlowerContinueApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowerContinueApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerContinueApplication.class, args);
+    }
 }
