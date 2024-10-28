b2628d6203a4cb3acf3784e6db34fb0d1b8d4466
hhafiya <149326005+hhafiya@users.noreply.github.com>
2024-10-28 20:35:45 +0200

    Update DHLDeliveryStrategy.java

--- src/main/java/com/flower/flowerсontinue/delivery/DHLDeliveryStrategy.java
+++ src/main/java/com/flower/flowerсontinue/delivery/DHLDeliveryStrategy.java
@@ -2,13 +2,16 @@ package com.flower.flowerсontinue.delivery;
 
 import com.flower.flowerсontinue.order.Order;
 
-public class DHLDeliveryStrategy implements Delivery{
+public class DHLDeliveryStrategy implements Delivery {
     @Override
     public String deliver(Order order) {
         if (order.isPaymentSuccessful()) {
-            return ("Delivering order #" + order.getOrderId() + " using DHL Delivery.");
+            return ("Delivering order #" + order.getOrderId() 
+                    + " using DHL Delivery.");
         } else {
-            return ("Cannot deliver order #" + order.getOrderId() + " due to unsuccessful payment.");
+            return ("Cannot deliver order #" 
+                    + order.getOrderId() 
+                    + " due to unsuccessful payment.");
         }
     }
 }
